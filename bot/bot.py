757bad730ad6d3bf5401f81df4b8d838d0db3e13
stumburs <stumburs@inbox.lv>
2025-07-22 16:49:01 +0300

    Add bot functionality with Twitch API integration and config handling

---
+++ bot/bot.py
@@ -0,0 +1,53 @@
+from twitchAPI.chat import Chat, EventData, ChatMessage, ChatSub, ChatCommand
+from twitchAPI.type import AuthScope, ChatEvent
+from twitchAPI.oauth import UserAuthenticator
+from twitchAPI.twitch import Twitch
+import configparser
+import asyncio
+
+config = configparser.ConfigParser()
+
+# TODO: Change path
+config.read("bot/config.ini")
+
+APP_ID = config['AUTH']['ClientID']
+APP_SECRET = config['AUTH']['ClientSecret']
+USER_SCOPE = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT]
+TARGET_CHANNEL = config['AUTH']['TargetChannel']
+
+
+async def on_message(msg: ChatMessage):
+    print(f'{msg.user.display_name}: {msg.text}')
+
+
+async def on_ready(ready_event: EventData):
+    await ready_event.chat.join_room(TARGET_CHANNEL)
+
+    print(f'Bot succesfully connected to {TARGET_CHANNEL}')
+
+
+async def mark_command(cmd: ChatCommand):
+    await cmd.reply("Hello!")
+
+
+async def run_bot():
+    bot = await Twitch(app_id=APP_ID, app_secret=APP_SECRET)
+    auth = UserAuthenticator(bot, USER_SCOPE)
+    token, refresh_token = await auth.authenticate()
+    await bot.set_user_authentication(token, USER_SCOPE, refresh_token=refresh_token)
+
+    chat = await Chat(bot)
+
+    chat.register_event(ChatEvent.READY, on_ready)
+    chat.register_event(ChatEvent.MESSAGE, on_message)
+    chat.register_command('mark', mark_command)
+
+    chat.start()
+
+    try:
+        input('Press ENTER to stop\n')
+    finally:
+        chat.stop()
+        await bot.close()
+
+asyncio.run(run_bot())

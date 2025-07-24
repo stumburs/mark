0034d4f1871205dfe064780d69e4b09283bd63fc
stumburs <stumburs@inbox.lv>
2025-07-25 01:31:56 +0300

    Refactor run scripts to ensure virtual environment creation and dependency installation are clearly defined

--- run.bat
+++ run.bat
@@ -1,7 +1,17 @@
 @echo off
 
+:: Check if venv exists
+if not exist "bot\venv\Scripts\activate" (
+    echo Creating virtual environment...
+    python -m venv bot\venv
+)
+
 :: Activate venv
 call bot\venv\Scripts\activate
+
+:: Install dependencies
+echo Installing dependencies...
+pip install --upgrade -r  bot\requirements.txt
 
 :: Run the bot
 python bot\bot.py
--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# Check if venv exists
+if [ ! -f "bot/venv/bin/activate" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv bot/venv
+fi
+
 # Activate venv
 source bot/venv/bin/activate
+
+# Install dependencies
+echo "Installing dependencies..."
+pip install --upgrade -r bot/requirements.txt
 
 # Run the bot
 python3 bot/bot.py
